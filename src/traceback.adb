f64fd48cb9cc3eb850854b176dfea5431b87705c
Simon Wright <simon@pushface.org>
2024-11-30 23:04:17 +0000

    For CI, force the test execution to terminate with success.
      * src/traceback.adb: catch & report exception in Symbolic_Traceback.

--- src/traceback.adb
+++ src/traceback.adb
@@ -9,8 +9,16 @@ exception
    when E : others =>
       Put_Line ("--- Ada.Exceptions.Exception_Information ---");
       Put_Line (Ada.Exceptions.Exception_Information (E));
-      Put_Line ("--- GNAT.Traceback.Symbolic.Symbolic_Traceback ---");
+   Put_Line ("--- GNAT.Traceback.Symbolic.Symbolic_Traceback ---");
+
+   --  Catch any exceptions raised in the software under development.
+   begin
       Put_Line
         (GNAT.Traceback.Symbolic.Symbolic_Traceback
            (Ada.Exceptions.Traceback.Tracebacks (E)));
+   exception
+      when E : others =>
+         Put_Line ("exception in Symbolic_Traceback: " &
+            Ada.Exceptions.Exception_Name (E));
+   end;
 end Traceback;
